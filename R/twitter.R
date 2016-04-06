7611b745119063ce7634ba52d09416695bd0c56c
urielaero <aero.uriel@gmail.com>
2016-04-05 23:57:42 -0500

    support geocode

--- R/twitter.R
+++ R/twitter.R
@@ -5,8 +5,8 @@ api_key = Sys.getenv("TWITTER_API_KEY")
 api_secret = Sys.getenv("TWITTER_API_SECRET")
 access_token = Sys.getenv("TWITTER_ACCESS_TOKEN")
 access_token_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-get_tweets_df <- function(search, max) {
+get_tweets_df <- function(search, max, lang=NULL, geocode=NULL) {
     twitteR::setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
-    tweets = twitteR::searchTwitter(search, n=max, lang="es", resultType="mixed")
+    tweets = twitteR::searchTwitter(search, n=max, lang=lang, resultType="mixed", geocode=geocode)
     twitteR::twListToDF(tweets)
 }
